8e5fdf44590c187ef032f411b0124edbf9c3ba23
Chengyu Bao <cjbxb0302@gmail.com>
2022-05-25 16:33:24 -0400

    deploy github page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,7 +12,12 @@ function About() {
     <Container fluid className="about-section">
       <Particle />
       <Container>
-        <Row style={{ justifyContent: "center", padding: "10px" }}>
+        <Row
+          style={{
+            justifyContent: "center",
+            padding: "10px",
+          }}
+        >
           <Col
             md={7}
             style={{
@@ -21,17 +26,30 @@ function About() {
               paddingBottom: "50px",
             }}
           >
-            <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-              Know Who <strong className="purple">I'M</strong>
+            <h1
+              style={{
+                fontSize: "2.1em",
+                paddingBottom: "20px",
+              }}
+            >
+              Know Who{" "}
+              <strong className="purple">I'M</strong>
             </h1>
             <Aboutcard />
           </Col>
           <Col
             md={5}
-            style={{ paddingTop: "120px", paddingBottom: "50px" }}
+            style={{
+              paddingTop: "120px",
+              paddingBottom: "50px",
+            }}
             className="about-img"
           >
-            <img src={laptopImg} alt="about" className="img-fluid" />
+            <img
+              src={laptopImg}
+              alt="about"
+              className="img-fluid"
+            />
           </Col>
         </Row>
 
--- src/components/Home/HomeHello.js
+++ src/components/Home/HomeHello.js
@@ -1,44 +1,54 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/home-main.svg";
+import homeLogo from "../../Assets/home.png";
 import Particle from "../Particle";
 import Type from "./Type";
 
 function HomeHello() {
   return (
-      <Container fluid className="home-section" id="home">
-        <Particle />
-        <Container className="home-content">
-          <Row>
-            <Col md={7} className="home-header">
-              <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hi There!{" "}
-                <span className="wave" role="img" aria-labelledby="wave">
-                  👋🏻
-                </span>
-              </h1>
+    <Container fluid className="home-section" id="home">
+      <Particle />
+      <Container className="home-content">
+        <Row>
+          <Col md={7} className="home-header">
+            <h1
+              style={{ paddingBottom: 15 }}
+              className="heading"
+            >
+              Hi There!{" "}
+              <span
+                className="wave"
+                role="img"
+                aria-labelledby="wave"
+              >
+                👋🏻
+              </span>
+            </h1>
 
-              <h1 className="heading-name">
-                I'M
-                <strong className="purple"> CHENGYU BAO</strong>
-              </h1>
+            <h1 className="heading-name">
+              I'M
+              <strong className="purple">
+                {" "}
+                CHENGYU BAO
+              </strong>
+            </h1>
 
-              <div style={{ padding: 50, textAlign: "left" }}>
-                <Type />
-              </div>
-            </Col>
+            <div style={{ padding: 50, textAlign: "left" }}>
+              <Type />
+            </div>
+          </Col>
 
-            <Col md={5} style={{ paddingBottom: 20 }}>
-              <img
-                src={homeLogo}
-                alt="home pic"
-                className="img-fluid"
-                style={{ maxHeight: "450px" }}
-              />
-            </Col>
-          </Row>
-        </Container>
+          <Col md={5} style={{ paddingTop: 75 }}>
+            <img
+              src={homeLogo}
+              alt="home pic"
+              className="img-fluid"
+              style={{ maxHeight: "700px" }}
+            />
+          </Col>
+        </Row>
       </Container>
+    </Container>
   );
 }
 
